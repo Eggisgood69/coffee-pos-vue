dc4c0ca47611e9f0f5f490c91980d891c84e6755
Jeff Lee <162120638+Eggisgood69@users.noreply.github.com>
2024-08-11 14:48:40 +0800

    add Transactions total orders

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -8,6 +8,9 @@ const props = defineProps(['transactions'])
 <template>
   <div>
     <h2 class="text-2xl font-bold mb-4">Transaction History</h2>
+    <div class="flex justify-end mb-2">
+    <div class="font-bold mr-4">Total Orders: {{ transactions.length}}</div>
+    </div>
     <div v-if="transactions.length === 0" class="text-gray-500">No transactions yet.</div>
     <TransactionItem
       v-for="transaction in transactions"
