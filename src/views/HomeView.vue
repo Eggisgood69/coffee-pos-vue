03428db913f0697ddd28605a9638f55cd9eab19b
Jeff Lee <162120638+Eggisgood69@users.noreply.github.com>
2024-08-11 14:23:00 +0800

    [Refactor] Refactor HoneView #2

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,51 @@
-<script setup></script>
+<script setup>
+import HomeViewSlot from './HomeViewSlot.vue'
+</script>
 
 <template>
   <div class="max-w-4xl mx-auto p-8">
-    <h1 class="text-4xl font-bold text-center mb-8">Welcome to Coffee Shop POS</h1>
+    <h1 class="text-4xl font-bold text-center mb-10">Welcome to Coffee Shop POS</h1>
     <div class="grid grid-col-1 md:grid-col-3 gap-6">
-      <RouterLink
-        to="/checkout"
-        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-6 px-4 rounded-lg text-center transition duration-300 ease-in-out transform hover:scale-105"
-      >
-        <i class="fas fa-cash-register text-4xl mb-2"></i>
-        <h2 class="text-2xl">Checkout</h2>
-        <p class="mt-2">Process orders and payments</p>
+      <RouterLink to="/checkout">
+        <HomeViewSlot>
+          <template #icon>
+            <i class="fas fa-cash-register text-4xl mb-2"></i>
+          </template>
+          <template #title>
+            <h2 class="text-3xl">Checkout</h2>
+          </template>
+          <template #description>
+            <p class="mt-2">Process orders and payments</p>
+          </template>
+        </HomeViewSlot>
       </RouterLink>
-      <RouterLink
-        to="/inventory"
-        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-6 px-4 rounded-lg text-center transition duration-300 ease-in-out transform hover:scale-105"
-      >
-        <i class="fas fa-cash-register text-4xl mb-2"></i>
-        <h2 class="text-2xl">Inventory</h2>
-        <p class="mt-2">Manage stock and items</p>
+
+      <RouterLink to="/inventory">
+        <HomeViewSlot>
+          <template #icon>
+            <i class="fas fa-box-open text-4xl mb-2"></i>
+          </template>
+          <template #title>
+            <h2 class="text-3xl">Inventory</h2>
+          </template>
+          <template #description>
+            <p class="mt-2">View and manage inventory</p>
+          </template>
+        </HomeViewSlot>
       </RouterLink>
-      <RouterLink
-        to="/transactions"
-        class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-6 px-4 rounded-lg text-center transition duration-300 ease-in-out transform hover:scale-105"
-      >
-        <i class="fas fa-cash-register text-4xl mb-2"></i>
-        <h2 class="text-2xl">Transactions</h2>
-        <p class="mt-2">View sales history</p>
+
+      <RouterLink to="/transactions">
+        <HomeViewSlot>
+          <template #icon>
+            <i class="fa-solid fa-clock-rotate-left text-4xl mb-2"></i>
+          </template>
+          <template #title>
+            <h2 class="text-3xl">Transactions</h2>
+          </template>
+          <template #description>
+            <p class="mt-2">View sales history</p>
+          </template>
+        </HomeViewSlot>
       </RouterLink>
     </div>
   </div>
---
+++ src/views/HomeViewSlot.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded-lg text-center transition duration-300 ease-in-out transform hover:scale-105">
+    <slot name="icon"></slot>
+    <slot name="title"></slot>
+    <slot name="description"></slot>
+  </div>
+</template>
+
